68c2acec99879dfb60ae872780aac747c2530cbb
Yash <yashghogre100@gmail.com>
2024-06-22 23:37:57 +0530

    fixed backward methods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#### Neural Networks in Go
+### Neural Networks in Go
 
 I know this is not the brightest idea but summer break makes you do things.
 Detailed docs coming soon...
--- nn.go
+++ nn.go
@@ -8,16 +8,16 @@ import (
 type Drop struct {
 	value     float32
 	grad      float32
-	prev      []Drop
+	prev      []*Drop
 	_backward func()
 }
 
-func (d Drop) getItem() {
+func (d *Drop) getItem() {
 	fmt.Println("( Drop: ", d.value, ")")
 }
 
-func (d *Drop) add(other *Drop) Drop {
-	out := Drop{value: (d.value + other.value), prev: []Drop{*d, *other}}
+func (d *Drop) add(other *Drop) *Drop {
+	out := &Drop{value: (d.value + other.value), prev: []*Drop{d, other}}
 
 	_backward := func() {
 		d.grad += out.grad
@@ -27,58 +27,52 @@ func (d *Drop) add(other *Drop) Drop {
 	return out
 }
 
-func (d *Drop) mul(other *Drop) Drop {
-	out := Drop{value: (d.value * other.value), prev: []Drop{*d, *other}}
+func (d *Drop) mul(other *Drop) *Drop {
+	out := &Drop{value: (d.value * other.value), prev: []*Drop{d, other}}
 
 	_backward := func() {
-		d.grad = out.grad * other.value
-		other.grad = out.grad * d.value
+		d.grad += out.grad * other.value
+		other.grad += out.grad * d.value
 	}
 	out._backward = _backward
 	return out
 }
 
-func (d *Drop) sub(other *Drop) Drop {
-	out := Drop{value: (d.value - other.value), prev: []Drop{*d, *other}}
+func (d *Drop) sub(other *Drop) *Drop {
+	out := &Drop{value: (d.value - other.value), prev: []*Drop{d, other}}
 
 	_backward := func() {
 		d.grad += out.grad
-		other.grad += out.grad
+		other.grad -= out.grad
 	}
 	out._backward = _backward
 	return out
 }
 
-func (d *Drop) div(other *Drop) Drop {
-	out := Drop{value: (d.value / other.value), prev: []Drop{*d, *other}}
+func (d *Drop) div(other *Drop) *Drop {
+	out := &Drop{value: (d.value / other.value), prev: []*Drop{d, other}}
 
 	_backward := func() {
 		d.grad = out.grad * other.value
-		other.grad = out.grad * d.value
+		other.grad = -out.grad * d.value / (other.value * other.value)
 	}
 	out._backward = _backward
 	return out
 }
-
-// func (d *Drop) pow(other *Drop) Drop {
-// 	out := Drop{value: float32(math.Pow(float64(d.value), float64(other.value))), prev: []Drop{*d, *other}}
-
-// 	_backward := func() {
-// 		d.grad =
-// 	}
-// 	return out
-// }
 
 func main() {
-	a := Drop{value: 4}
-	b := Drop{value: 5}
-	c := Drop{value: 2}
+	a := &Drop{value: 4}
+	b := &Drop{value: 5}
+	c := &Drop{value: 2}
 
-	var result_0 = (a.mul(&b))
-	var result_1 = result_0.add(&c)
-	result_1.grad = 5
+	result_0 := a.mul(b)
+	result_1 := result_0.add(c)
+	result_1.grad = 1
 
 	result_1._backward()
+	result_0._backward()
 
-	fmt.Println(result_0.grad)
+	fmt.Println(a.grad)
+	fmt.Println(b.grad)
+	fmt.Println(c.grad)
 }
