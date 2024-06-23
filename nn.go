45da790fb4f3c233df3d29a6cab64651435617d5
Yash <yashghogre100@gmail.com>
2024-06-23 13:21:28 +0530

    added relu, sigmoid and tanh functions

--- nn.go
+++ nn.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "math"
+	"math"
 )
 
 type Drop struct {
@@ -60,19 +60,70 @@ func (d *Drop) div(other *Drop) *Drop {
 	return out
 }
 
+func (d *Drop) relu() *Drop {
+
+	var tmp float32
+
+	if d.value < 0 {
+		tmp = 0
+	} else {
+		tmp = d.value
+	}
+
+	out := &Drop{value: tmp, prev: []*Drop{d}}
+
+	_backward := func() {
+		if d.value > 0 {
+			d.grad += 1 * out.grad
+		}
+	}
+	out._backward = _backward
+	return out
+}
+
+func (d *Drop) tanh() *Drop {
+
+	var tmp = float32((math.Exp(float64(d.value)) - math.Exp(-float64(d.value))) / (math.Exp(float64(d.value)) + math.Exp(-float64(d.value))))
+	out := &Drop{value: tmp, prev: []*Drop{d}}
+
+	_backward := func() {
+		d.grad += out.grad * float32(1-math.Pow(float64(tmp), 2))
+	}
+	out._backward = _backward
+	return out
+}
+
+func (d *Drop) sigmoid() *Drop {
+
+	var tmp = float32(1 / (1 + math.Exp(-float64(d.value))))
+	out := &Drop{value: tmp, prev: []*Drop{d}}
+
+	_backward := func() {
+		d.grad += out.grad * (tmp * (1 - tmp))
+	}
+	out._backward = _backward
+	return out
+}
+
 func main() {
 	a := &Drop{value: 4}
-	b := &Drop{value: 5}
-	c := &Drop{value: 2}
+	// b := &Drop{value: 5}
+	// c := &Drop{value: 2}
 
-	result_0 := a.mul(b)
-	result_1 := result_0.add(c)
-	result_1.grad = 1
+	// result_0 := a.mul(b)
+	// result_1 := result_0.add(c)
+	// result_1.grad = 1
 
-	result_1._backward()
-	result_0._backward()
+	// result_1._backward()
+	// result_0._backward()
 
-	fmt.Println(a.grad)
-	fmt.Println(b.grad)
-	fmt.Println(c.grad)
+	// fmt.Println(a.grad)
+	// fmt.Println((*result_0.prev[0]).value)
+	// fmt.Println(c.grad)
+
+	// print((a.relu()))
+	var d = a.tanh()
+	d.grad = 10
+	d._backward()
+	fmt.Println(a)
 }
